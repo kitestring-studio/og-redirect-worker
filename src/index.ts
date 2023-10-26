c17d8c408e715067a2704e7a0819bc201827e0bc
Gabe Herbert <gh@gabeherbert.com>
2023-10-25 17:06:56 -0700

    first attempt at returning old og tags

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,37 @@ export default {
 		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
 		const url = new URL(request.url);
 
+		// get useragent
+		const userAgent = request.headers.get('user-agent') || '';
+
+		// if useragent contains facebookexternalhit, return a special response
+		if (userAgent.includes('facebookexternalhit')) {
+			// get the current request url and change build.domain.com to www.domain.com
+			const currentUrl = new URL(request.url);
+			const newUrl = currentUrl.hostname.replace('build', 'www');
+
+			// get the html from the newUrl
+			const response = await fetch(newUrl);
+			const html = await response.text();
+
+			// extract the og:title and og:image from the html
+			const ogTitle = html.match(/<meta property="og:title" content="(.*)" \/>/);
+			const ogImage = html.match(/<meta property="og:image" content="(.*)" \/>/);
+			console.log(ogTitle, ogImage);
+
+			return new Response(
+				`<html>
+					<head>
+						<meta property="og:url" content="${currentUrl}" />
+						<meta property="og:title" content="${ogTitle}" />
+						<meta property="og:image" content="${ogImage}" />
+					</head>
+					<body></body>
+				</html>`,
+				{ headers: { 'Content-Type': 'text/html' } }
+			);
+		}
+
 		// You can get pretty far with simple logic like if/switch-statements
 		switch (url.pathname) {
 			case '/redirect':
